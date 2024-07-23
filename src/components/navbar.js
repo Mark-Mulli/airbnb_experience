c62c2c3c0ed9fe7e1f95d46915c7ba4c8be50c89
Mark Mulli <markmulli001@gmail.com>
2024-07-23 16:16:57 +0300

    second commit

---
+++ src/components/navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import logo from "../images/airbnb 1.png"
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+             <img src= {logo} alt = "my_Image"></img>
+        </nav>
+    );
+}
